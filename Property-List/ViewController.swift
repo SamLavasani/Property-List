ec4a126c175dd3a8b9a0880882ea2c346727db40
Samuel Lavasani <samuel.lavasani@adlibris.com>
2019-05-29 10:32:04 +0200

    Initial Commit

---
+++ Property-List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Property-List
+//
+//  Created by Samuel on 2019-05-29.
+//  Copyright © 2019 Samuel Lavasani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
